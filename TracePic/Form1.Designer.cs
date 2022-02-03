73995e2115e660a1a2b17d8e4ecaaeabd5ba758f
Nejimon Raveendran <nejimon.raveendran@gmail.com>
2022-02-02 21:50:17 -0500

    initial commit

---
+++ TracePic/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace TracePic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openDlg = new System.Windows.Forms.OpenFileDialog();
+            this.nudOpacity = new System.Windows.Forms.NumericUpDown();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGrid = new System.Windows.Forms.Button();
+            this.nudGridCols = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudLineWidth = new System.Windows.Forms.NumericUpDown();
+            this.cmbColor = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOpacity)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGridCols)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLineWidth)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggle.Location = new System.Drawing.Point(242, 24);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(105, 43);
+            this.btnToggle.TabIndex = 0;
+            this.btnToggle.Text = "Set";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBox.Location = new System.Drawing.Point(12, 88);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(1251, 850);
+            this.picBox.TabIndex = 2;
+            this.picBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // openDlg
+            // 
+            this.openDlg.FileName = "openFileDialog1";
+            // 
+            // nudOpacity
+            // 
+            this.nudOpacity.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudOpacity.Location = new System.Drawing.Point(106, 31);
+            this.nudOpacity.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudOpacity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudOpacity.Name = "nudOpacity";
+            this.nudOpacity.Size = new System.Drawing.Size(120, 31);
+            this.nudOpacity.TabIndex = 4;
+            this.nudOpacity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudOpacity.ValueChanged += new System.EventHandler(this.nudOpacity_ValueChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(146, 80);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(145, 38);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(145, 38);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnGrid
+            // 
+            this.btnGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrid.Location = new System.Drawing.Point(511, 21);
+            this.btnGrid.Name = "btnGrid";
+            this.btnGrid.Size = new System.Drawing.Size(132, 42);
+            this.btnGrid.TabIndex = 5;
+            this.btnGrid.Text = "Apply Grid";
+            this.btnGrid.UseVisualStyleBackColor = true;
+            this.btnGrid.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nudGridCols
+            // 
+            this.nudGridCols.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudGridCols.Location = new System.Drawing.Point(105, 30);
+            this.nudGridCols.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudGridCols.Name = "nudGridCols";
+            this.nudGridCols.Size = new System.Drawing.Size(72, 31);
+            this.nudGridCols.TabIndex = 6;
+            this.nudGridCols.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "opacity";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "rows/cols";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(363, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "width";
+            // 
+            // nudLineWidth
+            // 
+            this.nudLineWidth.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudLineWidth.Location = new System.Drawing.Point(431, 30);
+            this.nudLineWidth.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudLineWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudLineWidth.Name = "nudLineWidth";
+            this.nudLineWidth.Size = new System.Drawing.Size(74, 31);
+            this.nudLineWidth.TabIndex = 9;
+            this.nudLineWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // cmbColor
+            // 
+            this.cmbColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColor.FormattingEnabled = true;
+            this.cmbColor.Items.AddRange(new object[] {
+            "Red",
+            "White",
+            "Black",
+            "Yellow",
+            "Blue"});
+            this.cmbColor.Location = new System.Drawing.Point(248, 30);
+            this.cmbColor.Name = "cmbColor";
+            this.cmbColor.Size = new System.Drawing.Size(109, 33);
+            this.cmbColor.TabIndex = 11;
+            this.cmbColor.Tag = "Red";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "color";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbColor);
+            this.groupBox1.Controls.Add(this.btnGrid);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.nudLineWidth);
+            this.groupBox1.Controls.Add(this.nudGridCols);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(592, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(671, 81);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grid";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnToggle);
+            this.groupBox2.Controls.Add(this.nudOpacity);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(223, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 80);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tracing";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1277, 950);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TracePic";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOpacity)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudGridCols)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLineWidth)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openDlg;
+        private System.Windows.Forms.NumericUpDown nudOpacity;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnGrid;
+        private System.Windows.Forms.NumericUpDown nudGridCols;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudLineWidth;
+        private System.Windows.Forms.ComboBox cmbColor;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ TracePic/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TracePic
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("dwmapi.dll")]
+        static extern void DwmExtendFrameIntoClientArea(IntPtr hWnd, ref int[] pMargins);
+
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_LAYERED = 0x80000;
+        const int WS_EX_TRANSPARENT = 0x20;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+            this.TopMost = true;
+            notifyIcon1.Icon = this.Icon;
+
+            cmbColor.SelectedIndex = 0;
+        }
+
+        int initialStyle;
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            initialStyle = GetWindowLong(this.Handle, GWL_EXSTYLE);
+            SetWindowLong(this.Handle, GWL_EXSTYLE, initialStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            openDlg.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            var dlgResult = openDlg.ShowDialog();
+            if (dlgResult != DialogResult.OK)
+                return;
+
+            picBox.SizeMode = PictureBoxSizeMode.Zoom;
+            picBox.ImageLocation = openDlg.FileName;
+        }
+
+        private void nudOpacity_ValueChanged(object sender, EventArgs e)
+        {
+            this.Opacity = Convert.ToDouble(nudOpacity.Value);
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetWindowLong(this.Handle, -20, initialStyle);
+            //nudOpacity.Value = 1;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string tempLocation = @"c:\tmp\TracePic";
+
+            if (!Directory.Exists(tempLocation))
+                Directory.CreateDirectory(tempLocation);
+
+            var curFileInfo = new FileInfo(picBox.ImageLocation);
+
+            string griddedFile = tempLocation + @"\" + curFileInfo.Name.ToLower().Replace(curFileInfo.Extension.ToLower(), "") + "-gridded" + curFileInfo.Extension;
+            string canvasFile = griddedFile.Replace("-gridded", "-canvas");
+
+            var sourceImage = picBox.Image;
+
+            Color lineColor;
+
+            switch (cmbColor.Text) 
+            {
+                case "Red": 
+                    lineColor = Color.Red;
+                    break;
+                case "Black":
+                    lineColor = Color.Black;
+                    break;
+                case "White":
+                    lineColor = Color.White;
+                    break;
+                case "Yellow":
+                    lineColor = Color.Yellow;
+                    break;
+                case "Blue":
+                    lineColor = Color.Blue;
+                    break;
+                default:
+                    lineColor = Color.Red;
+                    break;
+            }
+
+            createGriddedImage(sourceImage, Convert.ToInt32(nudGridCols.Value), (float) nudLineWidth.Value, lineColor, griddedFile);
+            createGriddedImage(sourceImage, Convert.ToInt32(nudGridCols.Value), (float) nudLineWidth.Value, lineColor, canvasFile, true);
+
+            picBox.ImageLocation = griddedFile;
+
+        }
+
+
+        private void createGriddedImage(Image sourceImage, int rowCols, float lineWidth, Color lineColor, string targetFileLocation, bool createCanvas = false)
+        {
+            int side = sourceImage.Width < sourceImage.Height ? sourceImage.Width : sourceImage.Height;
+            var unit = Convert.ToInt32(side / rowCols);
+
+
+            using (var targetBitmap = new Bitmap(sourceImage.Width, sourceImage.Height))
+            {
+                using (var g = Graphics.FromImage(targetBitmap))
+                {
+                    
+                    if (createCanvas)
+                    {
+                        var brush = new SolidBrush(Color.White);
+                        var rect = new Rectangle(0, 0, targetBitmap.Width, targetBitmap.Height);
+                        g.FillRectangle(brush, rect);
+                    }
+                    else
+                    {
+                        g.DrawImage(sourceImage, 0, 0, sourceImage.Width, sourceImage.Height);
+                    }
+
+                    //draw lines
+                    var pen = new Pen(lineColor, lineWidth);
+                    int startX, startY, endX = unit, endY;
+                    int counter;
+                    Font font = new Font("Arial", 10);
+                    var fontBrush = new SolidBrush(lineColor);
+
+
+                    startX = unit;
+                    endX = unit;
+                    startY = 0;
+                    endY = sourceImage.Height;
+                    counter = 1;
+                   
+                    while (startX < sourceImage.Width)
+                    {
+                        g.DrawLine(pen, startX, startY, endX, endY);
+                        g.DrawString(counter.ToString(), font, fontBrush, startX + 1, startY + 1);
+
+                        startX = startX + unit;
+                        endX = endX + unit;
+                        counter++;
+                    }
+
+                    startY = unit;
+                    endY = unit;
+                    startX = 0;
+                    endX = sourceImage.Width;
+                    counter = 1;
+
+                    while (startY < sourceImage.Height)
+                    {
+                        g.DrawLine(pen, startX, startY, endX, endY);
+                        g.DrawString(counter.ToString(), font, fontBrush, startX + 1, startY + 1);
+
+                        startY = startY + unit;
+                        endY = endY + unit;
+                        counter++;                   }
+
+                    //g.DrawLine(pen, 10, 0, 10, targetBitmap.Height);
+
+                    targetBitmap.Save(targetFileLocation);
+                }
+            }
+        }
+
+    }
+}
